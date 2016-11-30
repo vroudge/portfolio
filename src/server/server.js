84c23a8cca0360893ebf6b4f01adb0b1bbf198c6
vroudge <v.roudge@gmail.com>
2016-11-30 11:09:28 +0200

    add analytics

--- src/server/server.js
+++ src/server/server.js
@@ -59,7 +59,7 @@ const renderFullPage = (html, initialState) => {
           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
           })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
         
-          ga('create', 'UA-50717374-1', 'auto');
+          ga('create', 'UA-50717374-3', 'auto');
           ga('send', 'pageview');
         
         </script>
